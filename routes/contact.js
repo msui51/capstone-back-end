5dbd6012bfef80d10d2ce751101cbc8fccf560d7
Maria Sui <maria.sui51@gmail.com>
2023-08-23 13:43:58 -0400

    added validation for post contact form route

--- routes/contact.js
+++ routes/contact.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const config = require('../knexfile.js');
 const knex = require('knex')(config);
 const { v4: uuidv4 } = require('uuid');
+const { body,validationResult } = require('express-validator');
 
 router
-    .route('/')
-    .post((req,res)=>{
+    .post('/',
+    body('name').notEmpty().withMessage('Please provide a name'),
+    body('email_address').notEmpty().isEmail().withMessage('Please provide a valid email address'),
+    body('message').notEmpty().withMessage('Please provide a message'),
+    (req,res)=>{
+        const errors = validationResult(req).formatWith(error => error.msg);
+        console.log(errors)
+        if(!errors.isEmpty()){
+             return res.status(400).json({errors: errors.array()})
+        }
         knex("contact_us")
             .insert({
                 id: uuidv4(),

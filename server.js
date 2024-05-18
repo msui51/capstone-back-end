0a3c5b83fe73ce6e100c59a264411f38de569f6d
Maria Sui <maria.sui51@gmail.com>
2024-05-18 14:24:34 -0400

    changed socket.io cors origin from localhost 3000 to heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server (server, {
     cors:{
-        origin:'http://localhost:3000'
+        origin:'https://capstone-back-end-8abd12be2c11.herokuapp.com/'
     }
 });
 

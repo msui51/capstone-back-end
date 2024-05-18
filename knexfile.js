284b7cb7ba0811a3db5b15d6c95c02d228caf8fb
Maria Sui <maria.sui51@gmail.com>
2024-05-18 12:40:54 -0400

    changed knex from local host to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
 module.exports = {
     client: 'mysql',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'rootroot',
-      database: 'capstone',
+      host: 'us-cluster-east-01.k8s.cleardb.net',
+      user: 'b6601e564037db',
+      password: 'bdd8a4fe',
+      database: 'heroku_98d1ba981a2d4d6',
       charset: 'utf8',
     },
   };
+
+  

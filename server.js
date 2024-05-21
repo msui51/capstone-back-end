631589a8a7167b4729b7c0c1e4fc4ee52a509cf9
Maria Sui <maria.sui51@gmail.com>
2024-05-21 10:36:17 -0400

    deleted extra code that isnt needed. chat works on netlify now

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-// const PORT = process.env.PORT;
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-// const { instrument } = require("@socket.io/admin-ui");
-// instrument(io, {
-//     auth: false,
-//     mode: "development",
-//   });
+
 
 
 // creating socket.io server
@@ -42,13 +37,6 @@ io.on('connection', (socket)=>{
         console.log(users);
    })
 })
-
-io.on("connection_error", (err) => {
-    console.log(err.req);      // the request object
-    console.log(err.code);     // the error code, for example 1
-    console.log(err.message);  // the error message, for example "Session ID unknown"
-    console.log(err.context);  // some additional error context
-  });
 
 
 // router for genres of music and show list

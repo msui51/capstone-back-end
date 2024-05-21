46c38c2c498107b447e36edc95189929c8fb3c38
Maria Sui <maria.sui51@gmail.com>
2024-05-21 10:26:00 -0400

    deleted the word engine

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ io.on('connection', (socket)=>{
    })
 })
 
-io.engine.on("connection_error", (err) => {
+io.on("connection_error", (err) => {
     console.log(err.req);      // the request object
     console.log(err.code);     // the error code, for example 1
     console.log(err.message);  // the error message, for example "Session ID unknown"
